ebfd0a24d140fd84822b71a2133faa71f2947150
zhenyashvyrkov <zhenya100694@mail.ru>
2022-11-02 18:14:51 +0300

    add seminar-3

---
+++ src/main/java/reactor/seminar3/classroom/FluxTraining.java
@@ -0,0 +1,16 @@
+package reactor.seminar3.classroom;
+
+public class FluxTraining {
+    public static void main(String[] args) {
+        fluxCreate();
+        fluxGenerate();
+    }
+
+    public static void fluxCreate() {
+
+    }
+
+    public static void fluxGenerate() {
+
+    }
+}
