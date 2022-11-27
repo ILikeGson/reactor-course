df33e6df1ad95299b7430ac4080845cb2c477574
zhenyashvyrkov <zhenya100694@mail.ru>
2022-11-27 23:02:02 +0300

    add seminar-6

--- src/main/java/reactor/seminar6/Client.java
+++ src/main/java/reactor/seminar6/Client.java
@@ -1,11 +1,24 @@
 package reactor.seminar6;
 
+import reactor.core.publisher.Flux;
 import reactor.seminar6.service.FootballService;
+import reactor.utils.Subscribers;
 
 public class Client {
     public static void main(String[] args) throws InterruptedException {
         FootballService footballService = new FootballService();
 
+
+        // Cold Publisher -> one to one
+        // Hot Publisher -> one to many
+
+        Flux<String> flux = footballService.broadcast()
+                .cache();
+
+        flux.subscribe(Subscribers.namedSubscriber("Lesha"));
+        Thread.sleep(10000L);
+
+        flux.subscribe(Subscribers.namedSubscriber("Igor"));
         Thread.sleep(10000L);
     }
 }
